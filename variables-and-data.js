6c5eb88b59c81edffc320ad795a046f3cc1b6cb3
Scott Wells <efwells101@gmail.com>
2018-12-13 20:29:50 -0700

    create .js file

---
+++ variables-and-data.js
@@ -0,0 +1,26 @@
+/*
+let obj = {
+	key1: "value",
+	key2: 4,
+	boolean: true,
+	obj2: {
+		obj2Key1: "value of inner object",
+	},
+};
+
+
+let var1 = "2";
+let var2 = 5;
+let result = var1 + var2;
+
+console.log(result);
+*/
+
+let add = 1 + 2;
+let sub = 2 -1;
+let mult = 2 * 4;
+let division = 4/2;
+let mod = 5 % 2;
+
+let string = "String 1 " + "String 2";
+ console.log(string);
